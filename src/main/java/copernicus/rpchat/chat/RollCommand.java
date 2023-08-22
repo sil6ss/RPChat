d23c3b948e7ddf780b5c1b4d2ac2c94f9f8b674d
sh6wn <singram9935@gmail.com>
2023-08-22 13:05:25 -0400

    Fixed Bug in /roll Command

--- src/main/java/copernicus/rpchat/chat/RollCommand.java
+++ src/main/java/copernicus/rpchat/chat/RollCommand.java
@@ -7,6 +7,10 @@ import org.bukkit.entity.*;
 
 import java.util.*;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.Integer.valueOf;
+
+
 public class RollCommand extends SubCommand {
 
     @Override
@@ -32,8 +36,20 @@ public class RollCommand extends SubCommand {
     @Override
     public void perform(CommandSender commandSender, String[] strings) {
         if (commandSender.hasPermission("rpchat.command.roll") || commandSender.hasPermission("rpchat.admin")) {
+            if (strings.length < 2) {
+                commandSender.sendMessage(ChatColor.RED + "Usage: " + getSyntax());
+                return;
+            }
+
+            int maxNumber = Integer.parseInt(strings[1]);
+            if (maxNumber <= 1) {
+                commandSender.sendMessage(ChatColor.RED + "Please specify a number greater than 1.");
+                return;
+            }
+
             Random random = new Random();
-            commandSender.sendMessage(ChatColor.GREEN + "You rolled a " + random.nextInt(0, Integer.parseInt(strings[strings.length - 1])) + "!");
+            int rolledNumber = random.nextInt(0, maxNumber + 1);
+            commandSender.sendMessage(ChatColor.GREEN + "You rolled a " + rolledNumber + "!");
         }
     }
 
